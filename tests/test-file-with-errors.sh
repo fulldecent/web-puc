dc16c9ee3e16ee89aeb2ecaa9ca8c5a7bd1c5572
William Entriken <github.com@phor.net>
2014-07-08 18:33:39 -0400

    Add new test

---
+++ tests/test-file-with-errors.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+WEBPUC=$BASEDIR/../web-puc.sh
+
+echo Creating temp file
+tempfoo=`basename $0`
+TMPFILE=`mktemp -q /tmp/${tempfoo}.XXXXXX`
+if [ $? -ne 0 ]; then
+    echo "$0: Can't create temp file, exiting..."
+    exit 1
+fi
+echo '<link href="//netdna.bootstrapcdn.com/bootstrap/2.3.0/css/bootstrap.css">' > $TMPFILE
+
+echo Running web-puc
+$WEBPUC $TMPFILE > /dev/null 2> /dev/null
+RESULT=$?
+rm $TMPFILE
+if [ $RESULT -eq "0" ]; then
+    echo Test failed: no error found in file with stale include
+    echo $RESULT
+    exit 1
+else
+    echo Test passed
+fi
+
